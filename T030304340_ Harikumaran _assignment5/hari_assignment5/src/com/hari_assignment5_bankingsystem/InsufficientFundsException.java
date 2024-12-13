cc75dc6d3d31f7c487295c75899a5552535dfe7e
Harikumaranv <girikumaran002@gmail.com>
2024-12-13 11:16:59 +0530

    Delete T030304340_ Harikumaran _assignment5 directory

--- T030304340_ Harikumaran _assignment5/hari_assignment5/src/com/hari_assignment5_bankingsystem/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.hari_assignment5_bankingsystem;
-
-
-class InsufficientFundsException extends Exception {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
--- T030304340_ Harikumaran _assignment5/hari_assignment5/src/com/hari_assignment5_bankingsystem/InvalidAmountException.java
+++
@@ -1,7 +0,0 @@
-package com.hari_assignment5_bankingsystem;
-
-class InvalidAmountException extends Exception {
-    public InvalidAmountException(String message) {
-        super(message);
-    }
-}
