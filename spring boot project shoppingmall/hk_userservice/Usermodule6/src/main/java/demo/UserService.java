0f82bc6eebb5e2c98731232e2a2d6bf219acba08
Harikumaranv <girikumaran002@gmail.com>
2024-12-13 11:23:22 +0530

    Add files via upload

---
+++ spring boot project shoppingmall/hk_userservice/Usermodule6/src/main/java/demo/UserService.java
@@ -0,0 +1,54 @@
+package demo;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    // Create a new user
+    public User createUser(User user) {
+        return userRepository.save(user);
+    }
+
+    // Get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    // Get user by ID
+    public User getUserById(Long id) throws UserNotFoundException {
+        Optional<User> user = userRepository.findById(id);
+        if (user.isPresent()) {
+            return user.get();
+        } else {
+            throw new UserNotFoundException("User not found with id: " + id);
+        }
+    }
+
+    // Update user details
+    public User updateUser(Long id, User updatedUser) throws UserNotFoundException {
+        return userRepository.findById(id).map(user -> {
+            user.setName(updatedUser.getName());
+            user.setEmail(updatedUser.getEmail());
+            user.setPhoneNumber(updatedUser.getPhoneNumber());
+            return userRepository.save(user);
+        }).orElseThrow(() -> new UserNotFoundException("User not found with id: " + id));
+    }
+
+    // Delete user by ID
+    public void deleteUser(Long id) throws UserNotFoundException {
+        if (userRepository.existsById(id)) {
+            userRepository.deleteById(id);
+        } else {
+            throw new UserNotFoundException("User not found with id: " + id);
+        }
+    }
+}
